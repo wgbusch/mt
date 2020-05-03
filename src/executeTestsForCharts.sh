d190d33a50fbf3e9a6892b30680a7d782020d8df
Ricardo Colombo <ricardogcolombo@gmail.com>
2020-05-03 19:27:57 -0300

    agrego wpl a analisis cuantitativo

--- src/executeTestsForCharts.sh
+++ src/executeTestsForCharts.sh
@@ -4,18 +4,25 @@ echo "Corriendo test CMM para test prob 1"
 echo " Corriendo test wp para test prob 1"
 ./tp ./tests/test-prob-1.in ./tests/testprob1chartwp.out 1
 
+echo " Corriendo test wpl para test prob 1"
+./tp ./tests/test-prob-1.in ./tests/testprob1chartwpl.out 3
+
 echo "Corriendo test CMM para test prob 2"
 ./tp ./tests/test-prob-2.in ./tests/testprob2chartcmm.out 0
 echo " Corriendo test wp para test prob 2"
 ./tp ./tests/test-prob-2.in ./tests/testprob2chartwp.out 1
+
+echo " Corriendo test wpl para test prob 2"
+./tp ./tests/test-prob-2.in ./tests/testprob2chartwpl.out 3
 
 echo "Corriendo test CMM para test 1"
 ./tp ./tests/test1.in ./tests/test1chartcmm.out 0
 echo " Corriendo test wp para test 1"
 ./tp ./tests/test1.in ./tests/test1chartwp.out 1
 
-
 echo "Corriendo test CMM para test 2"
 ./tp ./tests/test2.in ./tests/test2chartcmm.out 0
 echo " Corriendo test wp para test 2"
 ./tp ./tests/test2.in ./tests/test2chartwp.out 1
+
+
