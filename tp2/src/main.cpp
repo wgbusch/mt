be6b4823bc2ad9f3f563ba0bbccc6539a2f57c17
werner busch <w.g.busch@hotmail.com>
2020-05-31 01:43:02 -0300

    codigo para metodo de las potencias y del descenso

--- tp2/src/main.cpp
+++ tp2/src/main.cpp
@@ -6,9 +6,20 @@
 #include "pca.h"
 #include "eigen.h"
 
-int main(int argc, char** argv){
+void test_get_first_eigenvalues() {
+    Eigen::Matrix<double, 5, 1> v;
+    v << 5, 4, 3, 2, 1;
+    Eigen::Matrix<double, 5, 5> A = v.array().matrix().asDiagonal();
 
-  std::cout << "Hola mundo!" << std::endl;
+    std::pair<Vector, Matrix> result = get_first_eigenvalues(A, A.cols(), 100000, 1e-15);
 
-  return 0;
+    std::cout << "\n eigenvalues: \n" << result.first.array().round() << std::endl;
+    std::cout << "\n eigenvectors: \n" << result.second.array().round() << std::endl;
 }
+
+int main(int argc, char **argv) {
+    std::cout << "Hola mundo!" << std::endl;
+    test_get_first_eigenvalues();
+    return 0;
+}
+
