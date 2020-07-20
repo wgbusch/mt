789fe58eb133daaeae0ca27dbd0bbbeccdc82a73
Nicolas Hernandez <nicolas.carlos.hernandez@gmail.com>
2020-07-19 22:13:00 -0300

    Added quick tool to zip tp3

---
+++ packtp3.sh
@@ -0,0 +1,16 @@
+NAME="tp3-grupo14.zip"
+
+zip $NAME \
+tp3/CMakeLists.txt \
+tp3/README.md \
+tp3/notebooks/Elipses.ipynb \
+tp3/notebooks/PandasInmobiliario.ipynb \
+tp3/notebooks/PrediccionPrecios.ipynb \
+tp3/notebooks/util.py \
+tp3/requirements.txt \
+tp3/src/linear_regression.cpp \
+tp3/src/linear_regression.h \
+tp3/src/main.cpp \
+tp3/src/main_pybind.cpp \
+tp3/src/metnum.cpp \
+tp3/src/types.h
